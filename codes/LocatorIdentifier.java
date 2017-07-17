4c9b9cf82db47ff2940978b4094e77c4d739d0b6
BTES17 <ramandeep@btes.co.in>
2017-07-17 10:29:56 +0530

    Update LocatorIdentifier.java

--- codes/LocatorIdentifier.java
+++ codes/LocatorIdentifier.java
@@ -16,47 +16,9 @@ import com.google.common.base.Function;
 
 public class LocatorIdentifier {
 	
-	public static void testcase_DataDriven() throws IOException{
-		String path= "C:\\Documents and Settings\\install\\Desktop\\Sample.xls";
-		 Hls_Reader object=new Hls_Reader(path);
-		 String sheetname= object.getSheetName();
-		 driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		 int row_count=object.getRowCount(sheetname);
-		 int i;
-		 for(i=2;i<=row_count;i++)
-		 {
-			 open_fb();
-			 String Username=object.getCellData(sheetname, 1, i);
-			 String Password=object.getCellData(sheetname, 2, i);
-			 String expected_title= object.getCellData(sheetname,3,i);
-			 String tetscase_ID= object.getCellData(sheetname,0,i);
-			 System.out.println(Username+"  "+Password+"   " +expected_title);
-			 String actual_title=testscript_datadriven(Username, Password, expected_title);
-		       
-			 if( actual_title.equals(expected_title))
-		       {
-		      	 System.out.println("Test case is Failed");
-		       }
-		       else{
-		      	 System.out.println("Test case is Passed");
-		       }
-		 }
-		 	 
-	}
 	
-	static String testscript_datadriven(String Username, String Password, String expected_title)
-	{
-		WebElement element_email= driver.findElement(By.id("email"));
-        element_email.sendKeys("Raman");
-        WebElement element_pass= driver.findElement(By.id("pass"));
-        element_pass.sendKeys(Password);
-       WebElement element_login= driver.findElement(By.id("u_0_q"));
-       element_login.click();
-       
-       String actual_title= driver.getTitle();
-       return expected_title;
-	}
-   	
+	
+	
 
 	static WebDriver driver;
 	static void setup()
@@ -69,15 +31,17 @@ public class LocatorIdentifier {
 		 * The User. directory means the current working directory.
 		 *  */
 		String directory= System.getProperty("user.dir")+"\\src\\chromedriver.exe";
-	       
+	       /* The Chromedriver.exe file key and value have to be store in System class using setProperty method*/
         System.setProperty("webdriver.chrome.driver", directory);
        
 	}
+	
+	
 	static void open_fb()
 	{
 		 /*--- which means that the object (driver) is of type
 		  *  WebDriver but points to 
-		  * the memory allocation tall data and methods
+		  * the memory allocation to all data and methods
 		  *  in ChromeDriver
 		  */
 		 driver = new ChromeDriver();
@@ -87,65 +51,7 @@ public class LocatorIdentifier {
 		  * -*/
 	     driver.get("http://facebook.com/");
 	}
-	static void  Testcase1()
-	{
-		
-		WebElement element_email= driver.findElement(By.id("email"));
-        element_email.sendKeys("ABC");
-        WebElement element_pass= driver.findElement(By.id("pass"));
-        element_pass.sendKeys("ABC");
-       WebElement element_login= driver.findElement(By.id("u_0_q"));
-       element_login.click();
-       
-       String actual_title= driver.getTitle();
-       String expected_title= "Home";
-       
-     //  WebElement myDynamicElement = (new WebDriverWait(driver, 60)).until(ExpectedConditions.presenceOfElementLocated(By.xpath(".//*[@id='content1']/div/div[3]/img")));
-       
- /* if( actual_title.equals(expected_title))
-     {
-    	 System.out.println("Test case is Failed");
-     }
-     else{
-    	 System.out.println("Test case is Passed");
-     }*/
-       
-       
-     /*  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);//  
-      
-       
-       WebElement alt1= driver.findElement(By.cssSelector("div[class='_4rbf _53ij']"));
-      String  actual_errormsg=alt1.getText();
-      String expected_errormessage= "The email address or phone number that you've entered doesn't match any account.";
-      
-       if (actual_errormsg.contains(expected_errormessage))
-       {
-           System.out.println(" Test case is passed");
-         
-       }
-       else{
-      	 System.out.println("Test case is failed");
-       }*/
-       
-       WebElement fluent_elemnt=fluent();
-       String  actual_errormsg=fluent_elemnt.getText();
-       System.out.println(actual_errormsg);
-      
-	}
 	
-private static WebElement fluent() {
-    	
-        FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
-                .withTimeout(100, TimeUnit.SECONDS)
-                .pollingEvery(5, TimeUnit.MILLISECONDS)
-                .ignoring(NoSuchElementException.class);
-
-    return wait.until(new Function<WebDriver, WebElement>() {
-     public WebElement apply(WebDriver webDriver) {
-     return driver.findElement(By.cssSelector("div[class='_4rbf _53ij']"));
-            }
-        }); 
-    }
  static void ID ()
  {
 	 /*-- Webelement is an interface that contain the 
@@ -164,7 +70,8 @@ private static WebElement fluent() {
 		 * WebElement.
 		 * 
 		 */
-	 WebElement element_email= driver.findElement(By.id("email"));
+	 WebElement element_email= driver.findElement(By.id("email")); // id method take the value of the id attribute of 
+	 //a webElement which is to be located
 	 /*-- sendkeys and other methhods of Webelement Interface 
 	  * send the input to the element.
 	  */
@@ -172,20 +79,25 @@ private static WebElement fluent() {
  }
  static void name ()
  {
-	 WebElement element_email= driver.findElement(By.name("email"));
+	 WebElement element_email= driver.findElement(By.name("email")); // name method take the value of the name attribute
+	 // of a webElement which is to be located.
      element_email.sendKeys("ABC");
  }
  static void XPATH ()
  {
-	 WebElement element_email= driver.findElement(By.xpath(".//input[@id='email'][@name='email']"));
+	 WebElement element_email= driver.findElement(By.xpath(".//input[@id='email'][@name='email']")); 
+	 // xpath method take absolute or relative path of the webelement which is to be located.
      element_email.sendKeys("ABC");
  }
  static void CSSSelector ()
  {
-	 WebElement element_email= driver.findElement(By.cssSelector("input[id='email']"));
+	 WebElement element_email= driver.findElement(By.cssSelector("input[id='email' name='email']"));
+	 // cssselector method take tagname and the attributes of the webelement which is to be located.
      element_email.sendKeys("ABC");
  }
- static void Classname ()
+ 
+	
+static void Classname ()
  {
 List<WebElement> elements= driver.findElements(By.tagName("label"));
 for(WebElement e: elements)
@@ -196,7 +108,8 @@ for(WebElement e: elements)
 String abc=driver.findElements(By.tagName("label")).get(0).getText();
 System.out.println(abc);
  }
- static void link ()
+ 
+	static void link ()
  {
 	 WebElement element_email= driver.findElement(By.linkText("Forgotten account?"));
      element_email.click();
@@ -216,7 +129,7 @@ System.out.println(abc);
 		
 		setup();
 		open_fb();
-		 testcase_DataDriven();
+		 XPATH();
        
         
         
